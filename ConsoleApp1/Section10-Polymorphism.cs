a7d950adc64524d263cb33bfa58448899a7fef9f
brent-a <brent.k.aronsen@emcins.com>
2022-12-08 10:13:52 -0600

    Continue section 10 - more on abstract classes and interfaces

--- ConsoleApp1/Section10-Polymorphism.cs
+++ ConsoleApp1/Section10-Polymorphism.cs
@@ -11,19 +11,61 @@ namespace CSharpMasterClass
             //Example1();
         }
 
+        // Notes on interfaces vs abstract classes
+
+        // Definition:
+        // abstract class - incomplete class (can't be instantiated) provides a blueprint for derived classes
+        // interfaces - contract. Defines an ability that a class has. Cannot contain method definitions - only declarations
+        //      use this when not all derived classes will have this ability
+
+        // Differences:
+        // interface
+        //      not implemented
+        //      can't have constructor
+        //      contains only method declaration (not definitions)
+        //      no fields
+        //      classes can implement more than one interface
+        //      classes must implement all of its members (properties, methods)
+        //      what an object CAN DO
+        // abstract class
+        //      either partially implemented or not implemented at all
+        //      can have constructors
+        //      may contain method definitions, fields
+        //      classes can extend (inherit) one class only
+        //      classes must implement abstract members only
+        //      what an object IS
+
         public static void AbstractExample()
         {
             // Shape shape1 = new Shape(); // can't do this because it is an abstract class
-            Cube cube1 = new Cube(4.45);
+            Cube cube = new Cube(4.45);
             // cube1.GetInfo();
-            Sphere sphere1 = new Sphere(3.89);
+            Sphere sphere = new Sphere(3.89);
             // sphere1.GetInfo();
 
-            Shape[] shapes = { cube1, sphere1 };
+            Shape[] shapes = { cube, sphere };
             foreach(Shape shape in shapes)
             {
                 shape.GetInfo();
+                
+                Cube iceCube = shape as Cube; //create as a Cube
+                if (iceCube == null)
+                {
+                    Console.WriteLine("This shape is not a cube.");
+                }
+
+                if(shape is Cube)
+                {
+                    Console.WriteLine("This shape is a cube.");
+                }
+
+                object cube1 = new Cube(7); // object which contains a Cube
+                Cube cube2 = (Cube)cube1; // cast it to a Cube
+
+                Console.WriteLine($"{cube2.Name} has a volume of {cube2.Volume()}");
+
             }
+
 
         }
 
